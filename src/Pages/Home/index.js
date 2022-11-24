a06431c3e9aecb1df08a20edf68b5b1a75708d03
ThaisFReis <reis.thaisf@gmail.com>
2022-11-24 11:25:21 -0300

    Tentando renderizar os produtos

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,41 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
 import Menu from '../Menu';
 
 function Home (){
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function getProducts(){
+            try{
+                const response = await axios.get('http://localhost:5000/products');
+                setProducts(response.data);
+            } catch (error){
+                console.log(error);
+            }
+        }
+        getProducts();
+    }, []);
+
+    //Render products
+    const renderProducts = () => {
+        return products.map((product, index) => {
+            return (
+                <div key={index}>
+                    <h3>{product.name}</h3>
+                    <img src={product.image} alt={product.name} />
+                    <p>{product.price}</p>
+                </div>
+            )
+        })
+    }
+
     return (
-        <div className='home'>
+        <div>
             <Menu />
-             <h1>Home</h1>
+            <h1>Home</h1>
+            {renderProducts()}
         </div>
     )
 }
--- src/Pages/Menu/index.js
+++ src/Pages/Menu/index.js
@@ -5,11 +5,18 @@ import '../../Assets/Styles/Menu.css';
 
 function Menu(){
     return (
-        <div className="menu">       
+        <div className="menu"> 
+            <div className="title">
+                <h1>Yu-Gi-Oh Store</h1>
+            </div>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/singup">Cadastro</Link></li>
             </ul>
+            <div className="user">
+                <ion-icon name="person-circle-outline"></ion-icon>
+                <Link to="/login">Login</Link>
+            </div>
         </div>
     )
 }
